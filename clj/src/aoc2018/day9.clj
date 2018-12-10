634c5ac539f3b9739511d58ca2067b1b0638028f
Felix Bechstein <f@ub0r.de>
2018-12-10 11:13:37 +0100

    day 9.2

--- clj/src/aoc2018/day9.clj
+++ clj/src/aoc2018/day9.clj
@@ -2,10 +2,10 @@
   (:require [clojure.set])
   (:require [clojure.string]))
 
-(defn parse [input]
+(defn parse [input factor]
   (let [parts (clojure.string/split input #" ")]
     {:num-players   (Integer/parseInt (first parts))
-     :last-marble   (Integer/parseInt (nth parts 6))
+     :last-marble   (* (Integer/parseInt (nth parts 6)) factor)
      :field         '(0 2 1)
      :position      1
      :next-player   3
@@ -26,12 +26,12 @@
         parts (split-at position (:field state))]
     (if (zero? (mod next-marble 23))
       (let [player-score (get (:player-scores state) next-player 0)
-            split-position (mod (- position 9) num-marbles)
-            parts (split-at split-position (:field state))
+            position (mod (- position 9) num-marbles)
+            parts (split-at position (:field state))
             removed-marble (first (second parts))]
         (assoc state :next-player (mod (inc next-player) (:num-players state))
                      :next-marble (inc next-marble)
-                     :position split-position
+                     :position position
                      :player-scores (assoc (:player-scores state) next-player (+ player-score next-marble removed-marble))
                      :field (concat (first parts) (rest (second parts)))))
       (assoc state :field (concat (first parts) [next-marble] (second parts))
@@ -40,11 +40,15 @@
                    :next-marble (inc next-marble)))))
 
 (defn run1 [input]
-  (loop [state (parse input)]
+  (loop [state (parse input 1)]
     ;(println state)
     (if (finished? state)
       (high-score state)
       (recur (place-marble state)))))
 
 (defn run2 [input]
-  0)
+  (loop [state (parse input 100)]
+    ;(println state)
+    (if (finished? state)
+      (high-score state)
+      (recur (place-marble state)))))
