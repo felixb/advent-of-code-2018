5aac3770af4d4c4c21e06217191f56ea19053da0
Felix Bechstein <f@ub0r.de>
2018-12-11 16:57:47 +0100

    speed up day 11.2

--- clj/src/aoc2018/day11.clj
+++ clj/src/aoc2018/day11.clj
@@ -33,40 +33,41 @@
 (defn create-grid [sn]
   (reduce merge (map (partial create-row sn) (range 1 301))))
 
-(defn create-square [grid l x y]
-  {(grid-key x y l) (power-of-sqare grid l x y)})
+(defn create-items-of-work-for-row [l x]
+  (map (fn [y] [x y l]) (range 1 (- 301 l))))
+
+(defn create-items-of-work [l]
+  (apply concat (map (partial create-items-of-work-for-row l) (range 1 (- 301 l)))))
+
+(defn create-items-of-work-for-all-sizes []
+  (apply concat (map create-items-of-work (range 1 300))))
+
+(defn power-of-item [grid [x y l]]
+  [(grid-key x y l) (power-of-sqare grid l x y)])
 
 (defn max-square [e1 e2]
-  (if (> (first (vals e1)) (first (vals e2)))
+  (if (> (second e1) (second e2))
     e1
-    e2))
+    (do
+      (println "current best choice:" (first e2) "with power" (second e2))
+      e2)))
 
-(defn max-square-debug [e1 e2]
-  (println e1 "vs" e2)
-  (if (> (first (vals e1)) (first (vals e2)))
-    e1
-    e2))
-
-(defn best-row [grid l x]
-  (reduce max-square (pmap (partial create-square grid l x) (range 1 (- 301 l)))))
-
-(defn best-square [grid l]
-  (reduce max-square-debug (map (partial best-row grid l) (range 1 (- 301 l)))))
+(defn eval-items [grid items]
+  (reduce max-square (pmap (partial power-of-item grid) items)))
 
 (defn run1 [input]
   (let [sn (parse input)
         grid (create-grid sn)
-        best (best-square grid 3)
-        sq (first (keys best))
+        items (create-items-of-work 3)
+        best (eval-items grid items)
+        sq (first best)
         parts (clojure.string/split sq #",")]
     (str (first parts) "," (second parts))))
-
-(defn best-size [grid]
-  (reduce max-square-debug (map (partial best-square grid) (range 1 300))))
 
 (defn run2 [input]
   (let [sn (parse input)
         grid (create-grid sn)
-        best (best-size grid)
+        items (create-items-of-work-for-all-sizes)
+        best (eval-items grid items)
         sq (first (keys best))]
     sq))
